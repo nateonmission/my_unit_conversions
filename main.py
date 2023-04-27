a685c8cef0471876275ea1c27e06e7588285bd5e
Nate Allen <nallen@directionsresearch.com>
2023-04-27 16:08:00 -0400

    added gasoline cost conversion

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ import os
 import math
 from fractions import Fraction
 import tracemalloc
+import requests
+import json
 
+from SECRETS import exchange_api_key
+
+INCHES_IN_FOOT = 12
 POUNDS_IN_STONE = 7
 OUNCES_IN_POUNDS = 16
+GRAMS_IN_OUNCES = 28.3495
+OUNCE_IN_GRAM = 0.03527
+KJ_IN_KCAL = 0.239
+KCAL_IN_KJ = 4.184
+GALLON_IN_LITER = 0.26417
+LITERS_IN_GALLON = 3.78541178
+ONE_BASE_UNIT = 1
 
 def menu():
     os.system('cls')
@@ -12,10 +24,10 @@ def menu():
     print("1. Temperature")
     print("2. Short Distances (Feet/Inches & Meters)")
     print("3. Long Distances (Miles & Kilometers)")
-    print("4. Weight (Stones/Pounds & Kilograms)") # TODO
+    print("4. Weight (Stones/Pounds & Kilograms)") 
     print("5. Volume (Cups/Pints/Gallons & Liters)") # TODO
     print("6. Speed (MPH & km/h") # TODO
-    print("7. Energy (Calories & Joules)") #TODO
+    print("7. Energy (Calories & Joules)")
     print("")
     print("*****  *****")
     print("")
@@ -34,6 +46,10 @@ def menu():
         long_distance_convert()
     elif response == "4":
         weight_convert()
+    elif response == "7":
+        energy_convert()
+    elif response == "XX":
+        gas_price_convert()
     elif response == "0":
         return 0
     else:
@@ -42,7 +58,7 @@ def menu():
         input("Press ENTER to continue.")
         return 1
 
-
+# 1
 def temp_convert():
     os.system('cls')
     print("***** F/C Conversion *****")
@@ -51,12 +67,12 @@ def temp_convert():
     print("")
     response = input("Please make a selection: ")
     if response == "1":
-        response = input("enter your Fahrenheit temperature: ")
+        response = input("Enter your Fahrenheit temperature: ")
         c = (( float(response)-32)*5)/9
         print("")
         print(f"{response} F is {c} C")
     elif response == "2":
-        response = input("enter your Celsius temperature: ")
+        response = input("Enter your Celsius temperature: ")
         f = ((float(response)*(9/5))+32)
         print("")
         print(f"{response} C is {f} F")
@@ -69,6 +85,7 @@ def temp_convert():
     input("Press ENTER to continue.")
     return 0
 
+# 2
 def short_distance_convert():
     os.system('cls')
     print("***** Short Distance Conversion *****")
@@ -77,9 +94,9 @@ def short_distance_convert():
     print("")
     response = input("Please make a selection: ")
     if response == "1":
-        feet = input("enter your Feet (enter 0 if there is none): ")
-        inches = input("enter your Whole Inches (enter 0 if there is none): ")
-        fraction = input("enter your Inch Fraction (enter 0 if there is none): ").split('/')
+        feet = input("Enter your Feet (enter 0 if there is none): ")
+        inches = input("Enter your Whole Inches (enter 0 if there is none): ")
+        fraction = input("Enter your Inch Fraction (enter 0 if there is none): ").split('/')
         if len(fraction) > 1:
             decimal_inch = float(fraction[0])/float(fraction[1])
         else:
@@ -92,11 +109,10 @@ def short_distance_convert():
     elif response == "2":
         meters = input("enter your distance/length in meters: ")
         total_in_inches = float(meters) / 0.0254
-        feet = math.floor(total_in_inches / 12)
-        total_inches = total_in_inches - (feet*12)
+        feet = math.floor(total_in_inches / INCHES_IN_FOOT)
+        total_inches = total_in_inches - (feet * INCHES_IN_FOOT)
         int_inches = int(total_inches) 
         fraction_inch = str(Fraction(round((total_inches - int_inches)/0.125)*0.125).limit_denominator())
-
 
         print("")
         print(f"{meters} m is {feet} ft {int(int_inches)} {fraction_inch} ins ")
@@ -110,6 +126,7 @@ def short_distance_convert():
     input("Press ENTER to continue.")
     return 0
 
+# 3
 def long_distance_convert():
     os.system('cls')
     print("***** Short Distance Conversion *****")
@@ -118,8 +135,8 @@ def long_distance_convert():
     print("")
     response = input("Please make a selection: ")
     if response == "1":
-        miles = input("enter your Miles (enter 0 if there is none): ")
-        fraction = input("enter your Fraction of a mile (enter 0 if there is none): ").split('/')
+        miles = input("Enter your Miles (enter 0 if there is none): ")
+        fraction = input("Enter your Fraction of a mile (enter 0 if there is none): ").split('/')
         if len(fraction) > 1:
             str_fraction = f" and {fraction[0]}/{fraction[1]} miles"
         else:
@@ -158,6 +175,7 @@ def long_distance_convert():
     input("Press ENTER to continue.")
     return 0
 
+# 4
 def weight_convert():
     os.system('cls')
     print("***** Weight Conversion *****")
@@ -168,9 +186,9 @@ def weight_convert():
     print("")
     response = input("Please make a selection: ")
     if response == "1":
-        stones = input("enter Stones (0 if not used): ")
-        pounds = input("enter Pounds (0 if none): ")
-        ounces = input("enter Ounces (0 if none): ")
+        stones = input("Enter Stones (0 if not used): ")
+        pounds = input("Enter Pounds (0 if none): ")
+        ounces = input("Enter Ounces (0 if none): ")
         decimal_pounds = int(pounds) + int(stones)*7 + int(ounces)*0.0625
         kg = round(decimal_pounds * 0.45359, 3)
         if float(stones) > 0:
@@ -205,6 +223,16 @@ def weight_convert():
         print("or")
         print(f"you have {display_decimal_pounds} pounds.")
 
+    elif response == "3":
+        ounces = input("Enter Ounces (0 if none): ")   
+        grams = round(int(ounces) * GRAMS_IN_OUNCES, 3)
+        print(f"{ounces} ounces is {grams} grams")
+
+    elif response == "4":
+        grams = input("Enter Grams (0 if none): ")
+        ounces = round(int(grams) * OUNCE_IN_GRAM, 3)
+        print(f"{grams} grams is {ounces} ounces")
+
     else:
         print("")
         print("Invalid input. please enter a number from the menu.")
@@ -214,7 +242,99 @@ def weight_convert():
     input("Press ENTER to continue.")
     return 0
 
+# 5
+def volume_convert():
+    pass
 
+# 6 
+def speed_conver():
+    pass
+
+# 7
+def energy_convert():
+    os.system('cls')
+    print("***** Energy Conversion *****")
+    print("")
+    print("-- Calories and Joules (both with capital letters) are actually 1000 calories and joules.")
+    print("-- Technically Calories and Joules are kilocalories (kcals) and kilojoules (kJ)")
+    print("")
+    print("1. Kcals to kJ")
+    print("2. kJ to Kcals")
+    print("")
+    response = input("Please make a selection: ")
+    if response == "1":
+        Kcals = input("Enter your Kcals amount: ")
+        kJ = round(float(Kcals) * KCAL_IN_KJ, 3)
+        print("")
+        print(f"{Kcals} Kcals is {kJ} kJ")
+    elif response == "2":
+        kJ = input("Enter your kJ amount: ")
+        Kcals = round(float(kJ) * KJ_IN_KCAL, 3)
+        print("")
+        print(f"{kJ} kJ is {Kcals} Kcals")
+    else:
+        print("")
+        print("Invalid input. please enter a number from the menu.")
+        input("Press ENTER to continue.")
+        temp_convert()
+    print("")
+    input("Press ENTER to continue.")
+    return 0
+
+def _get_rates(params: dict = {"base": "USD", "symbols": "EUR,GBP,CAD,AUD,MXN"}):
+    url = 'https://api.apilayer.com/exchangerates_data/latest'
+    payload = {}
+    headers = {"apikey": exchange_api_key}
+    params = params
+    print("LOGGER: Before")
+    res = requests.request("GET", url, params = params, headers = headers, data = payload)
+    print("LOGGER: After")
+    while not res:
+        print("working")
+    os.system('cls')
+    data = json.loads(res.text)
+    if data['success'] == True:
+        rates = data['rates']
+        return rates
+    else:
+        return {"status": "ERROR"}
+
+# XX
+def gas_price_convert():
+    os.system('cls')
+    print("***** Money/Unit Conversion *****")
+    print("")
+    print("1. USD per Gal to GBP/CAD/AUD/MXN per liter")
+    print("2. GBP/L to others")
+    print("3. CAD/L to others")
+    print("4. AUD/L to others")
+    print("5. MXN/L to others")
+    print("")
+    response = input("Please make a selection: ")
+    if response == "1":
+        rates = _get_rates()
+        USD_per_gallon = input("How much does a gallon of gas cost, in USD? ")
+        for currency, rate_per_dollar in rates.items():
+            price_per_liter = round(float(USD_per_gallon) * GALLON_IN_LITER / (ONE_BASE_UNIT / rate_per_dollar), 2)
+            print(f"${float(USD_per_gallon)} per gallon is {price_per_liter} {currency} per liter")
+
+    elif response == "2":
+        base_currency = "GBP"
+        params = {"base": base_currency, "symbols": "AUD,CAD,EUR,GBP,MXN,USD"}
+        print(base_currency)
+        rates = _get_rates(params)
+        print(rates)
+        rate_per_dollar = rates['USD']
+        cost_per_litre = input(f"How much does a litre of petrol cost, in {base_currency}? ")
+        dollars_per_gallon = round(float(cost_per_litre) * LITERS_IN_GALLON * rate_per_dollar, 2)
+        print(f"{float(cost_per_litre)} / litre is {dollars_per_gallon} USD per gallon")
+
+        for currency, x_rate in rates.items():
+            currency_per_liter = round(float(cost_per_litre) / ( ONE_BASE_UNIT / x_rate), 2)
+            print(f"{float(cost_per_litre)} / litre is {currency_per_liter} {currency} per liter")
+
+    # print(data)
+    input("Pause")
 
 
 def main():
